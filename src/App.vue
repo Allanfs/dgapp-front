412cb72743e3ee90a63ad4660710b0c2cd886bd2
Allan Neri Fernandes da Silva <allan.nerifs@gmail.com>
2019-04-22 23:17:26 -0300

    Conhecendo o vuetify. 1

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app id="app">
+    <v-toolbar>
+      <v-toolbar-side-icon></v-toolbar-side-icon>
+      <v-toolbar-title>Dom Gilittu's</v-toolbar-title>
+    </v-toolbar>
+  
+    
+  </v-app>
+</template>
+
+<script>
+import Navigation from './components/Navigation.vue'
+
+export default {
+  name: 'app',
+  components: {
+    'navigation': Navigation
+  },
+  data () {
+    return {
+      drawer: null
+    }
+  }
+}
+</script>
+
+<style>
+</style>
