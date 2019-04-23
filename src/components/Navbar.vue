9a91b61593ff9bdd8ea1d5666ff39a2d117008f7
Allan Neri Fernandes da Silva <allan.fernandes@dataprev.gov.br>
2019-04-23 10:39:13 -0300

    Adivionado navbar lateral

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -15,7 +15,18 @@
     </v-toolbar>
 
     <v-navigation-drawer app v-model="drawer" class="primary">
-      <p>test</p>
+      <v-list>
+        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
+          <v-list-tile-action>
+            <v-icon class="white--text">{{link.icon}}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title class="white--text">
+              {{link.text}}
+            </v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
 
   </nav>
@@ -25,7 +36,10 @@
 export default {
   data() {
     return {
-      drawer: false
+      drawer: false,
+      links: [
+        { icon: 'dashboard', text: 'Menu', route: '/'}
+      ]
     }
   }
 }
