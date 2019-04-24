ff5e56f5b514b32666c793e677cc7fdf3097d566
Allan Neri Fernandes da Silva <allan.nerifs@gmail.com>
2019-04-23 21:56:05 -0300

    Adicionado lista de itens de cadastro

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,21 +16,32 @@
 
     <v-navigation-drawer app v-model="drawer" class="primary">
 
-      <v-list>
+      <v-list dense>
 
-        <v-list-tile v-for="link in links" :key="link.text" router :to="link.route">
-          <!-- icone   -->
-          <v-list-tile-action>
-            <v-icon class="white--text">{{link.icon}}</v-icon>
-          </v-list-tile-action>
-          <!-- texto -->
-          <v-list-tile-content>
-            <v-list-tile-title class="white--text">
-              {{link.text}}
-            </v-list-tile-title>
-          </v-list-tile-content>
-          
-        </v-list-tile>
+        <v-list-group>
+           <template slot="activator">
+              <v-list-tile>
+                <v-list-tile-content>
+                  <v-list-tile-title>Cadastrar</v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+            </template>
+            
+            <v-list-tile  v-for="link in links" :key="link.text" router :to="link.route">
+              <!-- icone -->
+              <v-list-tile-action>
+                <v-icon class="white--text">{{link.icon}}</v-icon>
+              </v-list-tile-action>
+              <!-- texto -->
+              <v-list-tile-content>
+                <v-list-tile-title color="red" class="white--text">
+                  {{link.text}}
+                </v-list-tile-title>
+              </v-list-tile-content>
+              
+            </v-list-tile>
+
+        </v-list-group>
         
       </v-list>
       
@@ -45,7 +56,17 @@ export default {
     return {
       drawer: false,
       links: [
-        { icon: 'dashboard', text: 'Menu', route: '/'}
+        { icon: 'dashboard', text: 'Categoria', route: '/adicionar/categoria'},
+        { icon: 'person', text: 'Cliente', route: '/adicionar/cliente'},
+        { icon: 'add_location', text: 'Endereco', route: '/adicionar/endereco'},
+        { icon: 'playlist_add', text: 'Pedido', route: '/adicionar/pedido'},
+        { icon: 'local_pizza', text: 'Pizza', route: '/adicionar/pizza'},
+        { icon: 'fastfood', text: 'Produto', route: '/adicionar/produto'},
+        { icon: 'local_play', text: 'Promocao', route: '/adicionar/promocao'},
+        { icon: 'local_dining', text: 'Recheio', route: '/adicionar/recheio'},
+        { icon: 'restaurant', text: 'Sabor', route: '/adicionar/sabor'},
+        { icon: 'hdr_strong', text: 'Tamanho', route: '/adicionar/tamanho'},
+        { icon: 'phone', text: 'Telefone', route: '/adicionar/telefone'}
       ]
     }
   }
