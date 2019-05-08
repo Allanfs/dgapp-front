e971a4ce16e93ffa0aae196a8cf82a392ae8d179
Allan Neri Fernandes da Silva <allan.fernandes@dataprev.gov.br>
2019-05-08 11:06:41 -0300

    router
    configurando children

---
+++ src/components/cadastro.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-container fluid grid-list-md>
+      <v-alert
+        :value="mensagemAlerta.visivel"
+        @input="mensagemAlerta =false"
+        :type="mensagemAlerta.type"
+        dismissible
+        transition="fade-transition"
+      >{{mensagemAlerta.mensagem}}</v-alert>
+
+      <!-- aqui será exibido o conteudo de cadastro,
+      talvez até de listagem também-->
+      <router-view></router-view>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  mensagemAlerta: {
+    get: function() {
+      return this.$store.getters.getAlerta;
+    },
+    set: function(value) {
+      this.$store.commit(REMOVER_ALERTA, null, { root: true });
+    }
+  }
+};
+</script>
--- src/router.js
+++ src/router.js
@@ -1,70 +1,85 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "./views/Home.vue";
 
-import AddRecheio from './components/recheio/AddRecheio.vue'
-import ListarRecheio from './components/recheio/ListarRecheio.vue'
+import AddRecheio from "./components/recheio/AddRecheio.vue";
+import ListarRecheio from "./components/recheio/ListarRecheio.vue";
 
-import AddTamanho from './components/tamanho/AddTamanho.vue'
-import ListarTamanho from './components/tamanho/ListarTamanho.vue'
+import AddTamanho from "./components/tamanho/AddTamanho.vue";
+import ListarTamanho from "./components/tamanho/ListarTamanho.vue";
 
-import AddProduto from './components/produto/AddProduto.vue'
-import AddCliente from './components/cliente/AddCliente.vue'
-import AddSabor from './components/sabor/AddSabor.vue'
+import AddProduto from "./components/produto/AddProduto.vue";
+import AddCliente from "./components/cliente/AddCliente.vue";
+import AddSabor from "./components/sabor/AddSabor.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/adicionar/",
+      name: "adicionar",
+      component: Home,
+      children: [
+        {
+          name: "Cadastrar Recheio",
+          path: "recheio",
+          component: AddRecheio
+        },
+        {
+          name: "Cadastrar Tamanho",
+          path: "tamanho",
+          component: AddTamanho
+        },
+        {
+          name: "Cadastrar Produto",
+          path: "produto",
+          component: AddProduto
+        },
+        {
+          name: "Cadastrar Cliente",
+          path: "cliente",
+          component: AddCliente
+        },
+        {
+          name: "Cadastrar Sabor",
+          path: "sabor",
+          component: AddSabor
+        }
+      ]
+    },
+    {
+      path: "/listar/",
+      name: "listar",
+      // component: ,
+      children: [
+        {
+          name: "Listar Recheio",
+          path: "recheio",
+          component: ListarRecheio
+        },
+        {
+          name: "Listar Tamanho",
+          path: "tamanho",
+          component: ListarTamanho
+        }
+      ]
+    },
+    {
+      path: "/",
+      name: "home",
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    },
-    {
-      path: '/adicionar/recheio',
-      name: 'recheio',
-      component: AddRecheio
-    },
-    {
-      path: '/adicionar/tamanho',
-      name: 'tamanho',
-      component: AddTamanho
-    },
-    {
-      path: '/adicionar/produto',
-      name: 'produto',
-      component: AddProduto
-    },
-    {
-      path: '/adicionar/cliente',
-      name: 'cliente',
-      component: AddCliente
-    },
-    {
-      path: '/adicionar/sabor',
-      name: 'sabor',
-      component: AddSabor,
-    },
-    {
-      path: '/listar/recheio',
-      name: 'listarSabor',
-      component: ListarRecheio,
-    },
-    {
-      path: '/listar/tamanho',
-      name: 'listarTamanho',
-      component: ListarTamanho,
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
-})
+});
