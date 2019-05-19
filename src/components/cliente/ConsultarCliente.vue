0a263420701076199f517d239c7c6ea5ace6bb46
Allan Neri Fernandes da Silva <allan.nerifs@gmail.com>
2019-05-19 13:14:24 -0300

    alinhamento de codigo

--- src/components/cliente/ConsultarCliente.vue
+++ src/components/cliente/ConsultarCliente.vue
@@ -1,61 +1,70 @@
 <template>
-  <v-form>
-    <v-container>
-      <v-layout row wrap>
-    <v-radio-group v-model="radios" :mandatory="false" row>
-      <v-radio label="CPF" value="cpf"></v-radio>
-      <v-radio label="Telefone" value="telefone"></v-radio>
-    </v-radio-group>
-        <v-flex xs12>
-          <v-text-field
-            v-model="dado"
-            append-icon="search"
-            box
-            :label="obterLabel"
-            type="text"
-            :mask="obterMascara()"
-            @click:append="consultar"
-          ></v-text-field>
-        </v-flex>
-
-      </v-layout>
-    </v-container>
-  </v-form>
+  <v-card>
+    <v-card-text>
+      <v-form @submit.prevent="consultar">
+      <v-container>
+        <v-layout row wrap>
+          <v-radio-group v-model="radios" :mandatory="false" row>
+            <v-radio id="input-radio-cpf" label="CPF" value="cpf"></v-radio>
+            <v-radio id="input-radio-tel" label="Telefone" value="telefone"></v-radio>
+          </v-radio-group>
+          <v-flex xs12>
+            <v-text-field
+              id="input-busca-cliente"
+              v-model="dado"
+              append-icon="search"
+              box
+              :label="obterLabel"
+              type="text"
+              :mask="obterMascara()"
+              @click:append="consultar"
+            ></v-text-field>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 export default {
-  name: 'consultar-cliente',
+  name: "consultar-cliente",
   data: () => ({
-      dado: '',
-      radios:'telefone',
-    }),
+    dado: "",
+    radios: "telefone"
+  }),
 
-    computed: {
-      obterLabel () {
-        if (this.radios === "cpf") {
-          return 'Buscar por CPF'
-        }
-        if (this.radios === "telefone") {
-          return 'Buscar por número de telefone'
-        }
+  computed: {
+    obterLabel() {
+      if (this.radios === "cpf") {
+        return "Buscar por CPF";
       }
-    },
-
-    methods: {
-      obterMascara() {
-        if(this.radios === "cpf"){ return '###.###.###-##'}
-        if(this.radios === "telefone"){ return '#####-####'}
-
-      },
-      consultar () {
-        this.$store.dispatch('cliente/buscarCliente',{dado: this.dado, tipo: this.radios })
-        // this.marker = !this.marker
+      if (this.radios === "telefone") {
+        return "Buscar por número de telefone";
       }
     }
+  },
+
+  methods: {
+    obterMascara() {
+      if (this.radios === "cpf") {
+        return "###.###.###-##";
+      }
+      if (this.radios === "telefone") {
+        return "#####-####";
+      }
+    },
+    consultar() {
+      this.$store.dispatch("cliente/buscarCliente", {
+        dado: this.dado,
+        tipo: this.radios
+      });
+      // this.marker = !this.marker
+    }
   }
+};
 </script>
 
 <style>
-
 </style>
